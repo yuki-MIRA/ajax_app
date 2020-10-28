e5e224b38cd0bed02cd15f72fdfb3fe2b5f71015
yuki-MIRA <oyuuki0423@gmail.com>
2020-10-28 21:06:23 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,8 @@ class PostsController < ApplicationController
     else
       post.update(checked: true)
     end
-
     item = Post.find(params[:id])
     render json: { post: item }
   end
+
 end
